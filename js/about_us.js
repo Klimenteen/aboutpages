c70d34f1aaf610b1c0bbe8b989f45138c210bce0
Klimenteen <90541621+Klimenteen@users.noreply.github.com>
2022-03-20 13:08:46 +0700

    hovers added

--- js/about_us.js
+++ js/about_us.js
@@ -1,5 +1,8 @@
+const cards = document.getElementsByClassName("team-card");
+const teamInfo = document.querySelectorAll(".team-info");
+const card_container = document.querySelector('.term-card-container')
+
 if (window.matchMedia("(max-width: 600px)").matches) {
-    const cards = document.getElementsByClassName("team-card");
     const hideBtn = document.querySelector(".hide-btn");
     for (let i = 3; i < cards.length; i++) {
         cards[i].style.display = "none";
@@ -11,8 +14,30 @@ if (window.matchMedia("(max-width: 600px)").matches) {
                 cards[i].style.display = "flex";
             }
 
-
-            console.log(1);
+            // console.log(1);
         });
     }
 }
+
+
+let openedInfo = 0;
+[...cards].forEach((card) => {
+    card.addEventListener('click', function() {
+        if (card_container && cards.length > 0 && card.dataset.open == '0') {
+            // console.log(card.children);
+            card.children[3].style.display = 'block'
+            card.children[0].classList.add('active')
+            card.children[2].classList.add('active')
+
+
+            card.dataset.open = '1'
+        } else {
+            card.children[3].style.display = 'none'
+            card.children[0].classList.remove('active')
+            card.children[2].classList.remove('active')
+
+
+            card.dataset.open = '0'
+        }
+    })
+})
